47aa383f9969a27eb4c680f6eb06dd4db972fe5e
casing1 <ehgis2344@gmail.com>
2024-09-12 17:03:29 +0900

    Update: 업데이트 내용 누락으로 인해 내용 추가 : LoginController, SignUpController를 합쳐 AuthController로 변경

--- src/main/java/tools/project/StGuideBook/service/UserService.java
+++ src/main/java/tools/project/StGuideBook/service/UserService.java
@@ -1,8 +1,6 @@
 package tools.project.StGuideBook.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
