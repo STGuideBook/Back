2003c0866fed55b96834a173675366787bf0fb15
casing1 <ehgis2344@gmail.com>
2024-10-06 19:44:37 +0900

    정적 웹 임시 추가

---
+++ src/main/java/tools/project/StGuideBook/controller/main.java
@@ -0,0 +1,15 @@
+package tools.project.StGuideBook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class main {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "main"; // index.html 파일을 반환
+    }
+}
