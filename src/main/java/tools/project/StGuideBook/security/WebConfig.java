9920955d4979034e957a8070ddff275417a47cc9
casing1 <ehgis2344@gmail.com>
2024-10-09 15:55:33 +0900

    1

--- src/main/java/tools/project/StGuideBook/security/WebConfig.java
+++ src/main/java/tools/project/StGuideBook/security/WebConfig.java
@@ -14,8 +14,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedOrigins(List.of("https://stguidebook-649b3dde2d26.herokuapp.com")); // 허용할 출처
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://stguidebook-649b3dde2d26.herokuapp.com")); // 허용할 출처
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용할 HTTP 메서드
         configuration.setAllowedHeaders(List.of("*")); // 허용할 헤더
         configuration.setAllowCredentials(true); // 인증 정보 포함 허용
