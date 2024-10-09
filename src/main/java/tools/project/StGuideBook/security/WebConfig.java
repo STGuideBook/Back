34737d8eb5c6f24bab7e6ead43e305b665f68240
casing1 <ehgis2344@gmail.com>
2024-10-09 15:16:43 +0900

    Update: securtiy

--- src/main/java/tools/project/StGuideBook/security/WebConfig.java
+++ src/main/java/tools/project/StGuideBook/security/WebConfig.java
@@ -7,15 +7,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://stguidebook-649b3dde2d26.herokuapp.com")); // 허용할 출처
+        configuration.setAllowedOrigins(List.of("https://stguidebook-649b3dde2d26.herokuapp.com")); // 허용할 출처
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // 허용할 HTTP 메서드
-        configuration.setAllowedHeaders(Arrays.asList("*")); // 허용할 헤더
+        configuration.setAllowedHeaders(List.of("*")); // 허용할 헤더
         configuration.setAllowCredentials(true); // 인증 정보 포함 허용
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
