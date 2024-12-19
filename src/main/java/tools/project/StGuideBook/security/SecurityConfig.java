b48ee32cfb25632af749afe9a966c60f4aedc3a8
casing1 <ehgis2344@gmail.com>
2024-12-19 16:03:10 +0900

    cors open

--- src/main/java/tools/project/StGuideBook/security/SecurityConfig.java
+++ src/main/java/tools/project/StGuideBook/security/SecurityConfig.java
@@ -31,13 +31,14 @@ public class SecurityConfig {
 //        http
         http.cors(cors -> cors.configurationSource(corsConfigurationSource))
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers("/main").permitAll()
-                        .requestMatchers("/user/login", "/user/signup", "/user/check-username", "/user/status").permitAll()
-                        .requestMatchers("/tip_board/list/**").permitAll()
-                        .requestMatchers("/dorm_reviews/review_list/**").permitAll()
-                        .requestMatchers("restaurant_review/review_list/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated())
+//                        .requestMatchers("/main").permitAll()
+//                        .requestMatchers("/user/login", "/user/signup", "/user/check-username", "/user/status").permitAll()
+//                        .requestMatchers("/tip_board/list/**").permitAll()
+//                        .requestMatchers("/dorm_reviews/review_list/**").permitAll()
+//                        .requestMatchers("restaurant_review/review_list/**").permitAll()
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                        .anyRequest().authenticated())
+                        .requestMatchers("/**").permitAll())
         .csrf(AbstractHttpConfigurer::disable)
         .httpBasic(Customizer.withDefaults())
 
